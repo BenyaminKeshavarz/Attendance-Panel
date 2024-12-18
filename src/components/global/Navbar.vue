c4fee124064da204f4554d0f5ac8fc0f8142092d
Benyamin Keshavarz <beny.keshavarz2018@gmail.com>
2024-12-18 22:14:59 +0330

    [FEATURE] Create navbar component

---
+++ src/components/global/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="sticky top-0 bg-background border-b py-5 mb-5">
+    <nav class="flex justify-between items-center">
+      <span class="inline-flex items-center gap-2 font-extrabold text-3xl">
+        <IconUserList class="size-8 text-primary" />
+        Attendance
+      </span>
+
+      <div>
+        <ThemeSwitcher />
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import ThemeSwitcher from "@/components/tools/ThemeSwitcher.vue";
+import IconUserList from "@/components/icons/IconUserList.vue";
+</script>
