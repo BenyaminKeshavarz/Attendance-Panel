a11b482813d3c7de6c42acc6185c566ba008721a
Benyamin Keshavarz <beny.keshavarz2018@gmail.com>
2024-12-18 22:19:15 +0330

    [FEATURE] Create base of semester page

---
+++ src/components/tools/Card.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import IconArrow from "@/components/icons/IconArrow.vue";
+
+defineProps({
+  title: String,
+});
+</script>
+
+<template>
+  <Card class="group transition-colors duration-300 hover:bg-accent">
+    <CardHeader>
+      <div class="flex justify-between items-center">
+        <CardTitle>{{ title }}</CardTitle>
+        <IconArrow
+          class="size-6 rtl:rotate-180 transition-transform duration-500 group-hover:translate-x-2 rtl:group-hover:-translate-x-2"
+        />
+      </div>
+    </CardHeader>
+  </Card>
+</template>
---
+++ src/views/SemesterView.vue
@@ -0,0 +1,72 @@
+<template>
+  <main>
+    <ul class="space-y-3">
+      <li v-for="item in semesters" :key="'semester' + item.id">
+        <RouterLink :to="`/classes?semester=${item.id}`">
+          <Card :title="item.title" />
+        </RouterLink>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script setup>
+import Card from "@/components/tools/Card.vue";
+
+const semesters = [
+  {
+    id: 3991,
+    title: "Semester 3991",
+  },
+  {
+    id: 3992,
+    title: "Semester 3992",
+  },
+  {
+    id: 3993,
+    title: "Semester 3993",
+  },
+  // {
+  //   id: 4001,
+  //   title: "Semester 4001",
+  // },
+  // {
+  //   id: 4002,
+  //   title: "Semester 4002",
+  // },
+  // {
+  //   id: 4003,
+  //   title: "Semester 4003",
+  // },
+  // {
+  //   id: 4011,
+  //   title: "Semester 4011",
+  // },
+  // {
+  //   id: 4012,
+  //   title: "Semester 4012",
+  // },
+  // {
+  //   id: 4013,
+  //   title: "Semester 4013",
+  // },
+  // {
+  //   id: 4021,
+  //   title: "Semester 4021",
+  // },
+  // {
+  //   id: 4022,
+  //   title: "Semester 4022",
+  // },
+  // {
+  //   id: 4023,
+  //   title: "Semester 4023",
+  // },
+  // {
+  //   id: 4031,
+  //   title: "Semester 4031",
+  // },
+];
+</script>
+
+<style></style>
