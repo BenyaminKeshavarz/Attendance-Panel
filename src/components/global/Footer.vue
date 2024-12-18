04daff49d311a0fb308a3545d9a998b69db4f6f0
Benyamin Keshavarz <beny.keshavarz2018@gmail.com>
2024-12-18 23:31:19 +0330

    [FEATURE] Create footer component

---
+++ src/components/global/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="flex justify-between mt-5 py-5 border-t font-semibold">
+    <p>
+      Made by ❤️
+      <a class="underline" href="https://github.com/BenyaminKeshavarz" target="_blank"
+        >Benyamin</a
+      >
+    </p>
+
+    <p>&copy; {{ year }}</p>
+  </footer>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const initialYear = 2024;
+const currentYear = new Date().getFullYear();
+
+const year = computed(() =>
+  initialYear === currentYear ? initialYear : `${initialYear} - ${currentYear}`
+);
+</script>
